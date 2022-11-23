254c409a41f1421ae3b24c1b041e5de942b37ac2
020813 <113406823+020813@users.noreply.github.com>
2022-11-23 16:36:59 +0900

    before solution

---
+++ bingoBoard.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include "bingoBoard.h"
+
+#define BINGONUM_HOLE
+
+static int bingoBoard[N_SIZE][N_SIZE];
+static int numberStatus[N_SIZE*N_SIZE];
+
+
+
+
+int bingo_checkNum(int selNum)
+{
+	if (numberStatus[selNum-1] == BINGONUM_HOLE)
+	    return BINGO_NUMSTATUS_ABSENT;
+	    
+	return BINGO_NUMSTATUS_PRESENT;
+}
+
+void bingo_init(void)
+{
+	int i, j;
+	int cnt = 1;
+	
+	for (i=0;i<N_SIZE;i++)
+	   for (j=0;j<N_SIZE;j++)
+	   {
+	 	 if (cnt == 15)
+	 	 {
+	 	 	bingoBoard[i][j] = BINGONUM_HOLE;
+	 	 	cnt++;
+		  }
+		  else
+		  {
+		  	numberStatus[cnt-1] = i*N_SIZE + j;
+		  	bingoBoard[i][j] = cnt++;
+		  }
+	   }
+}
+
+void bingo_print(void)
+{
+	int i, j;
+	
+	printf("=====================================\n");
+	for (i=0;i<N_SIZE;i++) {
+		for (j=0;j<N_SIZE;j++) 
+		{
+			if (bingoBoard[i][j] == BINGONUM_HOLE)
+			 printf("X\t");
+			else
+			printf("%i\t", bingoBoard[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("=====================================\n\n");
+}
+
+void bingo_inputNum(int sel)
+{
+	int row, col;
+	row= numberStatus[sel-1]/N_SIZE;
+	col=numberStatus[sel-1]%N_SIZE;
+	
+	bingoBoard[row][col]=BINGONUM_HOLE;
+	numberStatus[sel-1] = BINGONUM_HOLE;
+}
+
+int bingo_countCpmpletedLine(void)
+{
+	int i, j;
+	int cnt = 0;
+	int checkBingo;
+	
+	
+	for (i=0;i<N_SIZE;i++)
+	{
+		checkBingo = 1;
+		for (j=0;j<N_SIZE;j++)
+			if (bingoBoard[i][j] > 0){
+				checkBingo=0;
+				break;
+			}
+		
+		if (checkBingo == 1)
+			cnt++;
+		
+	}
+	
+	for (j=0;j<N_SIZE;j++)
+	{
+		checkBingo = 1;
+		for (i=0;i<N_SIZE;i++)
+		     if (bingoBoard[i][j] > 0)
+		 {
+		 	checkBingo = 0;
+		 	break;
+		 }
+		 
+       if (checkBingo == 1)
+       cnt ++;
+	}
+	
+	checkBingo = 1;
+	for (i=0;i<N_SIZE;i++)
+	{
+		if (bingoBoard[i][j] > 0)
+		{
+			checkBingo = 0;
+			break;
+		}
+	}
+	if (checkBingo == 1)
+	    cnt ++;
+	    
+	    
+	checkBingo = 1;
+	for (i=o;i<N_SIZE;i++)
+	{
+		if (bingoBoard[i][N_SIZE - i - 1] > 0)
+		{
+			checkBingo = 0;
+			break;
+		}
+	}
+	if (checkBingo == 1)
+	    cnt ++;
+	    
+	return cnt;
+}
---
+++ bingoBoard.h
@@ -0,0 +1,12 @@
+#define N_LINE
+#define N_SIZE
+#define BINGO_NUMSTATUS_ABSENT
+#define BINGO_NUMSTATUS_PRESENT
+
+
+
+
+void bingo_init(void);
+void bingo_print(void);
+void bingo_inputNum(int sel);
+int bingo_countCpmpletedLine(void);
